3484c02ec24fae8ba49bf08e3a5a3ac2905c4fd9
skeks1225-cmyk <skeks1225@gmail.com>
2026-08-05 07:26:24 +0900

    fix: remove redundant plugin management block in settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,20 +1,9 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
-}
-
-plugins {
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
 include(":app")
